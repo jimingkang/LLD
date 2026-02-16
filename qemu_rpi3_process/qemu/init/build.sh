9837085003ed391876654cee47021fc68e4780fa
jimingkang <jimingkang@gmail.com>
2026-02-17 01:33:56 +0800

    lesson 1: demo and qemu env setup

--- qemu_rpi3_process/qemu/init/build.sh
+++
@@ -1,5 +0,0 @@
-aarch64-linux-gnu-gcc -g -O0 -fno-omit-frame-pointer -c start.s -o start.o
-aarch64-linux-gnu-gcc -g -O0 -fno-omit-frame-pointer -std=c99 -ffreestanding -mgeneral-regs-only  -c main.c
-aarch64-linux-gnu-ld -nostdlib -T link.lds -o init start.o main.o ../lib/lib.a
-aarch64-linux-gnu-objcopy -O binary init init.bin
-aarch64-linux-gnu-ld -r -b binary init.bin -o init_blob.o
